3c736c18d494ff960647f4fb48368cdd37ced72b
brianShan974 <2366910832@qq.com>
2024-01-26 18:35:07 +0000

    Modified settings for noevide.

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 -- neovide settings
 if vim.g.neovide then
   vim.g.neovide_fullscreen = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_curl = 1.0
+  vim.g.neovide_cursor_vfx_particle_density = 20.0
+  vim.g.neovide_cursor_vfx_particle_lifetime = 1.8
 end
 
 return {
